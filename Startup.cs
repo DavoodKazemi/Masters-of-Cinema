36ff3de7b8de77a0a3b760c5ecf925954c5b408e
Davood Kazemi <sacredcode1@gmail.com>
2022-01-04 22:30:29 +0330

    enable MVC in startup.cs

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,8 @@ namespace MastersOfCinema
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            
             services.AddDbContext<Context>(options =>
             options.UseSqlServer(configuration.GetConnectionString("ContextDev")));
         }
@@ -32,19 +34,19 @@ namespace MastersOfCinema
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+
+            //Display error information - remove it at the end
+            app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints(cfg =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                cfg.MapControllerRoute("Default",
+                    "/{controller}/{action}/{id?}",
+                    new { controller = "App", action = "Index" });
             });
         }
     }
