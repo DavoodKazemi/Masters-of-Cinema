fa7a1f23186157d4c09605056164bbf5bf95b57d
Davood Kazemi <sacredcode1@gmail.com>
2022-01-04 20:25:28 +0330

    create context class

---
+++ Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using MastersOfCinema.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MastersOfCinema.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Director> Directors { get; set; }
+    }
+}
