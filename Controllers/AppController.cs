eb93ec7a48aa2d391d89706cf07429c268835144
Davood Kazemi <sacredcode1@gmail.com>
2022-01-05 12:44:29 +0330

    add app controller and its index view

---
+++ Controllers/AppController.cs
@@ -0,0 +1,18 @@
+﻿using MastersOfCinema.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MastersOfCinema.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
