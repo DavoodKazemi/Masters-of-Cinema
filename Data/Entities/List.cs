7d4997d65d758cca78520ba38462fd79f4634be1
Davood Kazemi <sacredcode1@gmail.com>
2022-03-08 20:36:35 +0330

    add 2 entity for custom lists

---
+++ Data/Entities/List.cs
@@ -0,0 +1,18 @@
+﻿using MastersOfCinema.ViewModels;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MastersOfCinema.Data.Entities
+{
+    public class List
+    {
+        [Key]
+        public int Id { get; set; }
+        public User User { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
+
+//This entity (with help of ListMovies entity) is for storing
+//any custom list information (not logged movies list or watchlists)
---
+++ Data/Entities/ListMovies.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace MastersOfCinema.Data.Entities
+{
+    public class ListMovies
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ListId { get; set; }
+        public int MovieId { get; set; }
+    }
+}
+
+
+//This entity is for storing movies of any custom list (not logged movies list or watchlists)
