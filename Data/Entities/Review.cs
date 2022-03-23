5cb4ef36c8a01e04827a59ab0b1b8adc524be5c6
Davood Kazemi <sacredcode1@gmail.com>
2022-03-23 18:23:48 +0430

    add review entity class

---
+++ Data/Entities/Review.cs
@@ -0,0 +1,14 @@
+﻿using MastersOfCinema.ViewModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace MastersOfCinema.Data.Entities
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+        public User User { get; set; }
+        public int MovieId { get; set; }
+        public string ReviewText { get; set; }
+    }
+}
