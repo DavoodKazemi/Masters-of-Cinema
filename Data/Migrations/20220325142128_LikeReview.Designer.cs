a17fddd1d2b920a1839df5fb8d2526f56cf38c6d
Davood Kazemi <sacredcode1@gmail.com>
2022-03-25 18:54:58 +0430

    migration for like review

--- Data/Migrations/20220325142128_LikeReview.Designer.cs
+++ Data/Migrations/20220325142128_LikeReview.Designer.cs
@@ -4,14 +4,16 @@ using MastersOfCinema.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MastersOfCinema.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20220325142128_LikeReview")]
+    partial class LikeReview
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
